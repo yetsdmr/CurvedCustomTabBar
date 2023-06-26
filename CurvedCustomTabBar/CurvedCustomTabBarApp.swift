30074ee2e41ecb426b71c9dcaee7e777b77de88c
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-06-26 14:55:58 +0300

    Initial Commit

---
+++ CurvedCustomTabBar/CurvedCustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurvedCustomTabBarApp.swift
+//  CurvedCustomTabBar
+//
+//  Created by Yunus Emre Taşdemir on 26.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct CurvedCustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
